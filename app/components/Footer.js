6e8ba3f8c8a74d3ac63b0a2bc5a10f5b98f735b0
Singabe Djonouma <singabe-xd@hotmail.fr>
2025-03-18 14:50:38 +0100

    Fix: Correction des backlinks dans le footer et amélioration de l'affichage des noms de domaine

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -14,7 +14,7 @@ const Footer = () => {
               target="_blank"
               rel="noopener noreferrer"
             >
-              Jade Lavoie
+              Jade Lavoie OnlyFans
             </a>
           </div>
           
